a6aa3a7f5079cd6b1bf62fadfad45cb7c9e9212b
Menard Ysrael Cruz <ysrael214@gmail.com>
2018-08-15 01:04:35 +0800

    ajax call test

--- js/bill.js
+++ js/bill.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
     setTimeout(function(){
       $(".content .container").load("data/main-menu.html");
     }, 100);
-
 });
+
+
 
 $( ".bp-button" ).click(function() {
   $( ".site-container" ).toggleClass( "modal-active" );
@@ -36,4 +37,23 @@ $( window ).resize(function() {
   $( ".bp-button i" ).removeClass( "fa-times" );
   $( ".bp-button i" ).addClass( "fa-bars" );
 });
+
+ $( ".bp-card" ).click(function() {
+      var clickedcard = $(this).children(".title").children("strong").html();
+
+      if (clickedcard == "What is a Bill?"){
+          $(".content .container").load("data/whats-a-bill.html");
+      }
+      else{
+          $( ".site-container" ).toggleClass( "modal-active" );
+          $( ".site-container" ).toggleClass( "feature-inactive" );
+      }
+    });
+
+    $( ".bp-menu" ).click(function() {
+       $( ".site-container" ).addClass( "modal-active" );
+       $( ".site-container" ).addClass( "feature-inactive" );
+    });
+
+
 
