809903c956763004350bd89575e5b745ca68d878
Menard Ysrael Cruz <ysrael214@gmail.com>
2018-08-15 02:14:10 +0800

    fixed more arrow bugs

--- js/bill.js
+++ js/bill.js
@@ -23,16 +23,21 @@ $( document ).on("click", ".bp-button", function() {
     $( ".site-container" ).removeClass( "modal-active" );
     $( ".site-container" ).removeClass( "menu-active" );
     $( ".site-container" ).removeClass( "feature-inactive" );
-    $( ".bp-button i" ).removeClass( "fa-times" );
-    $( ".bp-button i" ).addClass( "fa-bars" );
+
+    if (visitedpage == false){
+      $( ".bp-button i" ).removeClass( "fa-times" );
+      $( ".bp-button i" ).addClass( "fa-bars" );
+    }
   });
 
   $( document ).on("click", ".modal-close .dev-card button", function() {
     $( ".site-container" ).removeClass( "modal-active" );
     $( ".site-container" ).removeClass( "menu-active" );
     $( ".site-container" ).removeClass( "feature-inactive" );
-    $( ".bp-button i" ).removeClass( "fa-times" );
-    $( ".bp-button i" ).addClass( "fa-bars" );
+    if (visitedpage == false){
+      $( ".bp-button i" ).removeClass( "fa-times" );
+      $( ".bp-button i" ).addClass( "fa-bars" );
+    }
   });
 
 
