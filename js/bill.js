9b15b808316e026a8a0f274dd921bd827f76f452
Menard Ysrael Cruz <ysrael214@gmail.com>
2018-08-15 01:10:39 +0800

    changed click events to onclick

--- js/bill.js
+++ js/bill.js
@@ -3,39 +3,39 @@ $(document).ready(function(){
       $(".content .container").load("data/main-menu.html");
     }, 100);
 
-    $( ".bp-button" ).click(function() {
-  $( ".site-container" ).toggleClass( "modal-active" );
-  $( ".site-container" ).toggleClass( "menu-active" );
-  $( ".bp-button i" ).toggleClass( "fa-times" );
-  $( ".bp-button i" ).toggleClass( "fa-bars" );
-});
+  $( ".bp-button" ).on("click", function() {
+    $( ".site-container" ).toggleClass( "modal-active" );
+    $( ".site-container" ).toggleClass( "menu-active" );
+    $( ".bp-button i" ).toggleClass( "fa-times" );
+    $( ".bp-button i" ).toggleClass( "fa-bars" );
+  });
 
-$( ".modal-close-bg" ).click(function() {
-  $( ".site-container" ).removeClass( "modal-active" );
-  $( ".site-container" ).removeClass( "menu-active" );
-  $( ".site-container" ).removeClass( "feature-inactive" );
-  $( ".bp-button i" ).removeClass( "fa-times" );
-  $( ".bp-button i" ).addClass( "fa-bars" );
-});
+  $( ".modal-close-bg" ).on("click", function() {
+    $( ".site-container" ).removeClass( "modal-active" );
+    $( ".site-container" ).removeClass( "menu-active" );
+    $( ".site-container" ).removeClass( "feature-inactive" );
+    $( ".bp-button i" ).removeClass( "fa-times" );
+    $( ".bp-button i" ).addClass( "fa-bars" );
+  });
 
-$( ".modal-close .dev-card button" ).click(function() {
-  $( ".site-container" ).removeClass( "modal-active" );
-  $( ".site-container" ).removeClass( "menu-active" );
-  $( ".site-container" ).removeClass( "feature-inactive" );
-  $( ".bp-button i" ).removeClass( "fa-times" );
-  $( ".bp-button i" ).addClass( "fa-bars" );
-});
+  $( ".modal-close .dev-card button" ).on("click", function() {
+    $( ".site-container" ).removeClass( "modal-active" );
+    $( ".site-container" ).removeClass( "menu-active" );
+    $( ".site-container" ).removeClass( "feature-inactive" );
+    $( ".bp-button i" ).removeClass( "fa-times" );
+    $( ".bp-button i" ).addClass( "fa-bars" );
+  });
 
 
-$( window ).resize(function() {
-  $( ".site-container" ).removeClass( "modal-active" );
-  $( ".site-container" ).removeClass( "menu-active" );
-  $( ".site-container" ).removeClass( "feature-inactive" );
-  $( ".bp-button i" ).removeClass( "fa-times" );
-  $( ".bp-button i" ).addClass( "fa-bars" );
-});
+  $( window ).resize(function() {
+    $( ".site-container" ).removeClass( "modal-active" );
+    $( ".site-container" ).removeClass( "menu-active" );
+    $( ".site-container" ).removeClass( "feature-inactive" );
+    $( ".bp-button i" ).removeClass( "fa-times" );
+    $( ".bp-button i" ).addClass( "fa-bars" );
+  });
 
- $( ".bp-card" ).click(function() {
+ $( ".bp-card" ).on("click", function() {
       var clickedcard = $(this).children(".title").children("strong").html();
 
       if (clickedcard == "What is a Bill?"){
@@ -47,7 +47,7 @@ $( window ).resize(function() {
       }
     });
 
-    $( ".bp-menu" ).click(function() {
+    $( ".bp-menu" ).on("click", function() {
        $( ".site-container" ).addClass( "modal-active" );
        $( ".site-container" ).addClass( "feature-inactive" );
     });
